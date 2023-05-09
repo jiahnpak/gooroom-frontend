cf572e3723f2df72f46d91ec060973113d00eb1a
clap_0 <clap-0@naver.com>
2023-05-09 13:02:49 +0900

    fix: JWT 만료 시의 동작 변경
    + 액세스 토큰 만료에 대한 판별을 403 에러에서 401 에러를 통해 하도록 변경한다.
    + 액세스 토큰 만료 시 리프레시 토큰을 헤더에 넣어 재요청하도록 변경한다.
    + 위에 대한 서버의 응답에 액세스 토큰이 포함되어 있으면 액세스 토큰을 갱신한다.
    + 리프레시 토큰 만료 시 로그인 페이지로 리다이렉트한다.
    + 서버로 보내는 JWT의 앞에 'Bearer '를 붙이도록 변경한다.

--- src/hooks/useInterceptedAxios.js
+++ src/hooks/useInterceptedAxios.js
@@ -2,43 +2,58 @@ import {useEffect} from 'react';
 import customAxios from 'utils/customAxios';
 import useAuthState, {useAuthDispatch} from './useAuth';
 import {getRefreshToken} from 'utils/RefreshToken';
+import useAlert from './useAlert';
+import {useNavigate} from 'react-router-dom';
+import {LOGIN} from 'constants/path';
 
 const useInterceptedAxios = () => {
   const authState = useAuthState();
   const authDispatch = useAuthDispatch();
+
+  const showAlert = useAlert();
+  const navigate = useNavigate();
 
   useEffect(() => {
     const requestIntercept = customAxios.interceptors.request.use(
       config => {
         // 모든 Request Header에 accessToken을 넣어주는 역할
         if (!config.headers['Authorization']) {
-          config.headers['Authorization'] = `${authState.accessToken}`;
+          config.headers['Authorization'] = `Bearer ${authState.accessToken}`;
         }
         return config;
       },
       error => Promise.reject(error),
     );
     const responseIntercept = customAxios.interceptors.response.use(
-      response => response,
-      async error => {
-        // 토큰이 만료되었을 때 새로운 토큰을 발급하는 역할
+      response => {
+        // useInterceptedAxios를 통한 요청에 대한 서버의 응답 데이터에 accessToken이 있다면 재발급된 것이므로 갱신
+        const data = JSON.parse(response?.data || '{}');
+        const accessToken = data['Authorization'];
+        if (accessToken) {
+          authDispatch({type: 'SET_TOKEN', token: accessToken});
+        }
+        return response;
+      },
+      error => {
         const prevRequest = error?.config;
-        if (error?.response?.status === 403 && !prevRequest?.sent) {
+        if (error?.response?.status === 401 && !prevRequest?.sent) {
+          // 토큰이 만료되었을 때 refreshToken을 헤더에 넣어서 제전송
           prevRequest.sent = true;
           const refreshToken = getRefreshToken();
-          const response = await customAxios.post(
-            '/auth/refresh',
-            {},
-            {
-              headers: {
-                ...customAxios.defaults.headers,
-                'Authorization-refresh': refreshToken,
-              },
-            },
-          );
-          const newaccessToken = JSON.parse(response).Authorization;
-          authDispatch({type: 'SET_TOKEN', token: newaccessToken});
-          prevRequest.headers['Authorization'] = `${newaccessToken}`;
+
+          // refresh token도 만료된 경우 로그인 페이지로 이동
+          if (!refreshToken) {
+            showAlert(
+              'light',
+              '토큰 허용 시간이 만료되었습니다. 개인정보보호를 위해 다시 로그인해주세요.',
+              3000,
+            );
+            return navigate(LOGIN);
+          }
+
+          prevRequest.headers[
+            'Authorization-refresh'
+          ] = `Bearer ${refreshToken}`;
           return customAxios(prevRequest);
         }
       },
@@ -48,6 +63,7 @@ const useInterceptedAxios = () => {
       customAxios.interceptors.request.eject(requestIntercept);
       customAxios.interceptors.response.eject(responseIntercept);
     };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [authState, authDispatch]);
 };
 
