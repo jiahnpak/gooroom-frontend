60e8d72b5972dd292fdbad3b072fdd8e1c728bab
clap_0 <clap-0@naver.com>
2023-05-11 20:42:38 +0900

    fix: 환경변수로 API URL에 서버 주소 추가

--- src/constants/apiUrls.js
+++ src/constants/apiUrls.js
@@ -1,6 +1,6 @@
 import {REDIRECT_URI_KAKAO, REDIRECT_URI_NAVER} from 'constants/path';
 
-export const API_LOGIN_EMAIL = '/login/email';
+export const API_LOGIN_EMAIL = `${process.env.REACT_APP_API_URL}/login/email`;
 
 const CLIENT_ID_KAKAO = process.env.REACT_APP_CLIENT_ID_KAKAO;
 export const API_LOGIN_KAKAO = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${CLIENT_ID_KAKAO}&redirect_uri=${REDIRECT_URI_KAKAO}`;
@@ -8,9 +8,9 @@ export const API_LOGIN_KAKAO = `https://kauth.kakao.com/oauth/authorize?response
 const CLIENT_ID_NAVER = process.env.REACT_APP_CLIENT_ID_NAVER;
 export const API_LOGIN_NAVER = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${CLIENT_ID_NAVER}&redirect_uri=${REDIRECT_URI_NAVER}`;
 
-export const API_SIGNUP_EMAIL = '/signup/email';
+export const API_SIGNUP_EMAIL = `${process.env.REACT_APP_API_URL}/signup/email`;
 
-export const API_USERS = '/users';
-export const API_USERS_LIFESTYLE = '/users/lifestyle';
+export const API_USERS = `${process.env.REACT_APP_API_URL}/users`;
+export const API_USERS_LIFESTYLE = `${process.env.REACT_APP_API_URL}/users/lifestyle`;
 
-export const API_USERS_PROFILEIMAGE = '/users/profileImage';
+export const API_USERS_PROFILEIMAGE = `${process.env.REACT_APP_API_URL}/users/profileImage`;
